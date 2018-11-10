d8babd65c55d34c5c38dd8a98c308284a6a0d041
Christian Castro <36249260+ChristianCastr0@users.noreply.github.com>
2018-11-10 08:01:14 -0800

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,49 +1,140 @@
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Stack;
 
-public class Graph {
-	//Vertex of graph
-	class Vertex{
-		LinkedList<Vertex> adjList;	//List of neighbors
-		int color;	//WHITE=0, GREY=1, BLACK=2
-		String value;	
-		int row;	//row of vertex
-		int col;	//column of vertex
+public class Graph {	
+	Vertex[][] graph;
+	//Vertex currentVert;
+	Vertex[] vertexList;
+	int dimension;
+	int total_Verts;
+	
+	//Constructor
+	public Graph(int dimension) {
+		this.dimension=dimension;
+		total_Verts=dimension*dimension;
+		//adjList=new Node[total_Verts];
 		
 		
-		public Vertex(int row, int col) {
-			this.row=row;
-			this.col=col;
-			this.value="";
-			adjList=new LinkedList<>();
-			color=0;
+		//Create and fill graph
+		graph=new Vertex[dimension][dimension];
+		vertexList=new Vertex[total_Verts];
+		int value=1;
+		for(int i=0;i<dimension;i++) {
+			for(int j=0;j<dimension;j++) {
+				Vertex v=new Vertex(value);
+				vertexList[value-1]=v;
+				graph[i][j]=v;
+				value++;
+			}
 		}
 	}
 	
-	private final int MAX_VERTS=20;
-	private Vertex vertexList[];
-	private int adjMatrix[][];
-	private int n;
+	/**
+	 * Sets the adjacency list for each vertex
+	 */
+	public void setNeighbors() {
+		for(int i=0;i<dimension;i++) {
+			for(int j=0;j<dimension;j++) {
+				//add top neighbor if not in first row
+				if(j!=0)
+					graph[i][j].adjList.add(graph[i][j-1]);
+				//add bottom neighbor if not on last row
+				if(j!=dimension-1)
+					graph[i][j].adjList.add(graph[i][j+1]);
+				//add left neighbor if not in first column of graph
+				if(i!=0)
+					graph[i][j].adjList.add(graph[i-1][j]);
+				//add right neighbor if not in last column
+				if(i!=dimension-1)
+					graph[i][j].adjList.add(graph[i+1][j]);
+			}
+		}
+	}
 	
-	public Graph(int dimension) {
-		vertexList=new Vertex[dimension*dimension];
-		adjMatrix= new int[dimension][dimension];
-		n=adjMatrix.length;
+	public void breakWall(Vertex current, Vertex neighbor) {
+		if(current.label+1==neighbor.label) {
+			current.RIGHT=false;
+			neighbor.LEFT=false;
+		}
+		else if(current.label-1==neighbor.label) {
+			current.LEFT=false;
+			neighbor.RIGHT=false;
+		}
+		else if(current.label+dimension==neighbor.label) {
+			current.BOTTOM=false;
+			neighbor.TOP=false;
+		}
+		else {
+			current.TOP=false;
+			neighbor.BOTTOM=false;
+		}		
+	}
+	
+	public void dfsMaze(Vertex current) {
+		Stack<Vertex> stack=new Stack<Vertex>();
+		int visited=0;	//Number of vertexes visited
 		
-		for(int i=0; i<MAX_VERTS; i++)
-			for(int j=0; j<MAX_VERTS;j++)
-				adjMatrix[i][j]=0;
+		while(visited<total_Verts) {
+			if(!current.adjList.isEmpty()) {	//if vertex has unvisited neighbors
+				Vertex neighbor=current.getNeighbor();
+				breakWall(current, neighbor);
+				stack.push(current);	//visit other neighbors if any
+				current=neighbor;	//Go to next node
+				visited++;
+			}
+			else {
+				current=stack.pop();
+			}		
+		}
 	}
 	
-	public void addVertex(char lab) {
-		vertexList[n++]=new Vertex(lab);
+	public void createMaze(Vertex v){
+		Queue<Vertex> q=new LinkedList<>();
+		q.add(v);
+		while(!q.isEmpty()) {
+			Vertex next=q.remove();
+			
+			for(int i=0;i<next.adjList.size()-1;i++) {
+				Vertex neighbor=next.adjList.get(i);
+				
+			}
+		}
 	}
 	
-	public void addEdge(int start, int end) {
-		adjMatrix[start][end]=1;
-		adjMatrix[end][start]=1;
+	public String printMaze() {
+		String maze="";
+		for(int i=0;i<total_Verts;i++) {
+
+		}
 	}
 	
-	public void displayVertex(int v) {
-		System.out.print(vertexList[v].label);
+	//Vertex of graph
+	static class Vertex{
+		LinkedList<Vertex> adjList;	//List of neighbors
+		int color;	//WHITE=0, GREY=1, BLACK=2
+		int label;	//Vertex number 1...n
+		boolean TOP=true;	//Walls
+		boolean RIGHT=true;
+		boolean BOTTOM=true;
+		boolean LEFT=true;
+		
+		public Vertex(int label) {
+			this.label=label;
+			color=0;
+		}
+		
+		public Vertex getNeighbor() {
+			//if white turn grey
+			if(color==0)
+				color=1;
+			
+			Random random=new Random();
+			Vertex neighbor=adjList.get(random.nextInt(adjList.size()-1));	//select random neighbor
+			adjList.remove(neighbor);	//remove from adjacency list
+			
+			return neighbor;
+		}
 	}
 }

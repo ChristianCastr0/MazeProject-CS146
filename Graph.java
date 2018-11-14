dde42f2d1a3f90799ea225d25c6fede01c4c5719
Christian Castro <36249260+ChristianCastr0@users.noreply.github.com>
2018-11-14 09:35:03 -0800

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -3,10 +3,13 @@ import java.util.Queue;
 import java.util.Random;
 import java.util.Stack;
 
+/**
+ * Program that represents a nXn graph that can be altered to represent a perfect maze
+ * @author Christian Castro & Hibo Osman
+ * @version 1.0	11/14/18
+ */
 public class Graph {	
 	Vertex[][] graph;
-	Vertex[] vertexList;
-	Vertex[][] adjList;
 	int dimension;
 	int total_Verts;
 	
@@ -14,27 +17,27 @@ public class Graph {
 	public Graph(int dimension) {
 		this.dimension=dimension;
 		total_Verts=dimension*dimension;
-		adjList=new Vertex[total_Verts][4];
-		
 		
 		//Create and fill graph
 		graph=new Vertex[dimension][dimension];
-		vertexList=new Vertex[total_Verts];
 		int value=1;
 		for(int i=0;i<dimension;i++) {
 			for(int j=0;j<dimension;j++) {
 				Vertex v=new Vertex(value);
-				vertexList[value-1]=v;
 				graph[i][j]=v;
 				value++;
 			}
 		}
+		//Break walls for start and end of maze
 		graph[0][0].TOP=false;
 		graph[dimension-1][dimension-1].BOTTOM=false;
-		
-		//graph[0][dimension-1].TOP=false;
 	}
 	
+	/**
+	 * Gets vertex in graph based on the given label
+	 * @param n	Label of the vertex
+	 * @return	Vertex v that corresponds to the label given in the parameter
+	 */
 	public Vertex getVertex(int n) {
 		Vertex v=null;
 		for(int i=0;i<dimension;i++) {
@@ -47,27 +50,31 @@ public class Graph {
 	}
 	
 	/**
-	 * Sets the adjacency list for each vertex
+	 * Sets the walls for each vertex
 	 */
 	public void setNeighbors() {
 		for(int i=0;i<dimension;i++) {
 			for(int j=0;j<dimension;j++) {
-				//add top neighbor if not in first row
+				//add top wall if not in first row
 				if(i!=0)
-					graph[i][j].adjList.add(graph[i-1][j]);
-				//add bottom neighbor if not on last row
+					graph[i][j].wallList.add(graph[i-1][j]);
+				//add bottom wall if not on last row
 				if(i!=(dimension-1))
-					graph[i][j].adjList.add(graph[i+1][j]);
-				//add left neighbor if not in first column of graph
+					graph[i][j].wallList.add(graph[i+1][j]);
+				//add left wall if not in first column of graph
 				if(j!=0)
-					graph[i][j].adjList.add(graph[i][j-1]);
-				//add right neighbor if not in last column
+					graph[i][j].wallList.add(graph[i][j-1]);
+				//add right wall if not in last column
 				if(j!=dimension-1)
-					graph[i][j].adjList.add(graph[i][j+1]);
+					graph[i][j].wallList.add(graph[i][j+1]);
 			}
 		}
 	}
 	
+	/**
+	 * Gets the adjacency list of each vertex in the graph
+	 * @return	List of adjacency lists for each vertex
+	 */
 	public String getAdjList() {
 		String list="";	
 		for(int i=0;i<dimension;i++) {
@@ -81,6 +88,11 @@ public class Graph {
 		return list;
 	}
 	
+	/**
+	 * Breaks wall between the 2 vertexes given in the parameter
+	 * @param current Current vertex
+	 * @param neighbor	Neighbor of the current vertex
+	 */
 	public void breakWall(Vertex current, Vertex neighbor) {
 		//break right neighbors wall
 		if(current.label+1==neighbor.label) {
@@ -104,13 +116,16 @@ public class Graph {
 		}	
 	}
 	
+	/**
+	 * Using the DFS method goes through each vertex and breaks walls to create a perfect maze
+	 */
 	public void createMaze() {
 		Stack<Vertex> stack=new Stack<>();
 		int visited=1;	//Number of vertexes visited
 		Vertex current=graph[0][0];
 		
 		while(visited<total_Verts) {
-			if(!current.adjList.isEmpty()&&current.whiteNeighbors()) {	//if vertex has neighbors
+			if(!current.wallList.isEmpty()&&current.whiteNeighbors()) {	//if vertex has neighbors
 				//System.out.print("a");
 				Vertex neighbor=current.getNeighbor();
 				breakWall(current, neighbor);
@@ -125,6 +140,9 @@ public class Graph {
 		}
 	}
 	
+	/**
+	 * Finds a path to the end of the maze using BFS method
+	 */
 	public void solveBFS(){
 		Vertex currentV = graph[0][0];  // first position
 		Queue<Vertex> q = new LinkedList<>();  
@@ -146,6 +164,9 @@ public class Graph {
 		}	
 	}
 	
+	/**
+	 * Finds path to the end of the maze using DFS method
+	 */
 	public void solveDFS() {
 		Vertex current=graph[0][0];	//starting point
 		Stack<Vertex> stack=new Stack<Vertex>();
@@ -154,7 +175,7 @@ public class Graph {
 		
 		while(!stack.isEmpty() && current.label!=total_Verts) {	//Stack is not empty and has not reached exit
 			current=stack.pop();	
-			current.color2=2;	//Make vertex black do its not visited again
+			current.color2=2;	//Make vertex black so it's not visited again
 			current.setStep(step);	//set step for this vertex
 			step+=1;	//increment step
 			
@@ -167,6 +188,10 @@ public class Graph {
 		}
 	}
 	
+	/**
+	 * Prints maze based on the status of each vertex's walls and step variable
+	 * @return	String that contains maze pattern
+	 */
 	public String printMaze() {
         String maze = "";
         int r=0;
@@ -205,7 +230,7 @@ public class Graph {
 	
 	//Vertex of graph
 	static class Vertex{
-		LinkedList<Vertex> adjList;	//List of neighbors
+		LinkedList<Vertex> wallList;	//List of walls
 		LinkedList<Vertex> adjList2;	//List of neighbors
 		int color;	//WHITE=0, GREY=1, BLACK=2
 		int color2;
@@ -217,7 +242,7 @@ public class Graph {
 		boolean LEFT=true;
 		
 		public Vertex(int label) {
-			this.adjList = new LinkedList<Vertex>();
+			this.wallList = new LinkedList<Vertex>();
 			this.adjList2 = new LinkedList<Vertex>();
 			this.label=label;
 			step=" ";
@@ -225,28 +250,44 @@ public class Graph {
 			color2=0;
 		}
 		
+		/**
+		 * Sets step of the vertex
+		 * @param n Number that represent the step tajen to get to this vertex
+		 */
 		public void setStep(int n) {
 			step=Integer.toString(n);
 		}
 		
+		/**
+		 * Gets the value of the step in the form of a string
+		 * @return Step
+		 */
 		public String getStep() {
 			return step;
 		}
 		
+		/**
+		 * Checks if the the current vertex has any white neighbors
+		 * @return True or false depending on if there are any white neighbors
+		 */
 		public boolean whiteNeighbors() {
-			for(int i=0;i<this.adjList.size();i++) {
-				if(adjList.get(i).color==0)
+			for(int i=0;i<this.wallList.size();i++) {
+				if(wallList.get(i).color==0)
 					return true;
 			}
 			return false;
 		}
 		
+		/**
+		 * Creates an array of white neighbors and then randomly selects one of them
+		 * @return A white neighbor
+		 */
 		public Vertex getNeighbor() {
 			Vertex[] white=new Vertex[4];	//create array list for white neighbors
 			int index=0;
-			for(int i=0;i<this.adjList.size();i++) {
-				if(!this.adjList.isEmpty()&&this.adjList.get(i).color==0) {
-					white[index]=this.adjList.get(i);
+			for(int i=0;i<this.wallList.size();i++) {
+				if(!this.wallList.isEmpty()&&this.wallList.get(i).color==0) {
+					white[index]=this.wallList.get(i);
 					index++;
 				}
 			}
@@ -264,9 +305,9 @@ public class Graph {
 	}
 	
 	public static void main(String[] args) {
-		Graph g = new Graph(3);
-		Graph g2 = new Graph(3);
-		Graph g3 = new Graph(3);
+		Graph g = new Graph(4);
+		Graph g2 = new Graph(4);
+		Graph g3 = new Graph(4);
 		System.out.println(g.getVertex(1).step);
 		g.setNeighbors();
 		System.out.println(g2.getVertex(1).step);
@@ -281,7 +322,7 @@ public class Graph {
 		g3.createMaze();
 		g.solveDFS();
 		g2.solveBFS();
-		g3.printHash();
+		//g3.printHash();
 		System.out.print(g.getAdjList());
 		System.out.print("DFS: \n"  + g.printMaze());
 		System.out.print("BFS: \n"  + g2.printMaze());

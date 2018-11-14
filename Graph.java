9547c5d994cdb4d96f74c13b1e5168aaf3acf9b4
Christian Castro <36249260+ChristianCastr0@users.noreply.github.com>
2018-11-13 16:45:50 -0800

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -6,6 +6,7 @@ import java.util.Stack;
 public class Graph {	
 	Vertex[][] graph;
 	Vertex[] vertexList;
+	Vertex[][] adjList;
 	int dimension;
 	int total_Verts;
 	
@@ -13,7 +14,7 @@ public class Graph {
 	public Graph(int dimension) {
 		this.dimension=dimension;
 		total_Verts=dimension*dimension;
-		//adjList=new Node[total_Verts];
+		adjList=new Vertex[total_Verts][4];
 		
 		
 		//Create and fill graph
@@ -32,6 +33,17 @@ public class Graph {
 		graph[dimension-1][dimension-1].BOTTOM=false;
 		
 		//graph[0][dimension-1].TOP=false;
+	}
+	
+	public Vertex getVertex(int n) {
+		Vertex v=null;
+		for(int i=0;i<dimension;i++) {
+			for(int j=0;j<dimension;j++) {
+				if(graph[i][j].label==n)
+					v=graph[i][j];
+			}
+		}
+		return v;
 	}
 	
 	/**
@@ -61,8 +73,8 @@ public class Graph {
 		for(int i=0;i<dimension;i++) {
 			for(int j=0;j<dimension;j++) {
 				Vertex v=graph[i][j];
-				for(int k=0;k<v.adjList.size();k++)
-					list+=v.adjList.get(k).label+" ";
+				for(int k=0;k<v.adjList2.size();k++)
+					list+=v.adjList2.get(k).label+" ";
 				list+="\n";
 			}
 		}
@@ -89,7 +101,7 @@ public class Graph {
 		else {
 			current.TOP=false;
 			neighbor.BOTTOM=false;
-		}		
+		}	
 	}
 	
 	public void createMaze() {
@@ -128,19 +140,19 @@ public class Graph {
 	
 	public void solveDFS() {
 		Vertex current=graph[0][0];	//starting point
-		Stack<Vertex> stack=new Stack<>();
+		Stack<Vertex> stack=new Stack<Vertex>();
 		stack.push(current);
 		int step=0;	//The number of steps taken to get to vertex
 		
-		while(!stack.isEmpty() && current.label!=vertexList.length-1) {	//Stack is not empty and has not reached exit
-			current=stack.pop();	//
-			current.color=2;	//Make vertex black do its not visited again
-			current.step=step;	//set step for this vertex
+		while(!stack.isEmpty() && current.label!=total_Verts) {	//Stack is not empty and has not reached exit
+			current=stack.pop();	
+			current.color2=2;	//Make vertex black do its not visited again
+			current.setStep(step);	//set step for this vertex
 			step+=1;	//increment step
 			
-			for(int i=0;i<current.adjList.size()-1;i++) {	//Look at each neighbor
-				Vertex next=current.adjList.get(i);
-				if(next.color==0) {	//If neighbor is white add it to stack
+			for(int i=0;i<current.adjList2.size()-1;i++) {	//Look at each neighbor
+				Vertex next=getVertex(current.adjList2.get(i).label);
+				if(next.color2==0) {	//If neighbor is white add it to stack
 					stack.push(next);
 				}
 			}
@@ -154,10 +166,10 @@ public class Graph {
 			for(int col=0;col<dimension;col++) {
 				if(row%2!=0) {	//if "|" must be printed
 					if(graph[r][col].LEFT)
-						maze+="|"+graph[r][col].label;
+						maze+="|"+graph[r][col].step;
 					else
-						maze+=" "+graph[r][col].label;
-					if(col==dimension-1/*&&graph[r][col].RIGHT*/) {
+						maze+=" "+graph[r][col].step;
+					if(col==dimension-1) {
 						maze+="|";
 						r++;
 					}
@@ -176,9 +188,6 @@ public class Graph {
 						maze+="+ ";
 				}
 			}
-			/*r++;
-			if(r==dimension)
-				r=0;*/
 			if(row%2==0)
 				maze+="+";
 			maze+="\n";
@@ -189,9 +198,11 @@ public class Graph {
 	//Vertex of graph
 	static class Vertex{
 		LinkedList<Vertex> adjList;	//List of neighbors
+		LinkedList<Vertex> adjList2;	//List of neighbors
 		int color;	//WHITE=0, GREY=1, BLACK=2
+		int color2;
 		int label;	//Vertex number 1...n
-		int step;	//Steps taken to get to  this vertex
+		String step=" ";	//Steps taken to get to  this vertex
 		boolean TOP=true;	//Walls
 		boolean RIGHT=true;
 		boolean BOTTOM=true;
@@ -199,8 +210,19 @@ public class Graph {
 		
 		public Vertex(int label) {
 			this.adjList = new LinkedList<Vertex>();
+			this.adjList2 = new LinkedList<Vertex>();
 			this.label=label;
+			step="1";
 			color=0;
+			color2=0;
+		}
+		
+		public void setStep(int n) {
+			step=Integer.toString(n);
+		}
+		
+		public String getStep() {
+			return step;
 		}
 		
 		public boolean whiteNeighbors() {
@@ -224,6 +246,8 @@ public class Graph {
 			Random random=new Random();
 			Vertex neighbor=white[random.nextInt(index)];	//select random white neighbor
 			//Update adjacency list and color of both vertices
+			adjList2.add(neighbor);
+			neighbor.adjList2.add(this);
 			neighbor.color=1;
 			this.color=1;
 			
@@ -233,11 +257,15 @@ public class Graph {
 	
 	public static void main(String[] args) {
 		Graph g = new Graph(3);
+		System.out.println(g.getVertex(1).step);
+		g.getVertex(1).setStep(3);
+		System.out.println(g.getVertex(1).step);
 		g.setNeighbors();
 		System.out.print(g.getAdjList());
 		System.out.println(g.printMaze());
 		
 		g.createMaze();
+		g.solveDFS();
 		System.out.print(g.getAdjList());
 		System.out.print(g.printMaze());
 	}
